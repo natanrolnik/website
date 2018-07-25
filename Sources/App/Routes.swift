41bae736ca17fe25c0c5ed849f02248cf9b9fca4
Natan Rolnik <natanrolnik@gmail.com>
2018-07-25 16:10:16 +0300

    Updates routes

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -2,9 +2,14 @@ import Vapor
 
 private let blogRedirects = [
     "2014/03/26/my-intersection/": "my-intersection",
-    "2016/06/05/wwdc-16-past-and-future/": "wwdc16-past-and-future",
-    "2015/08/09/empathy/": "empathy",
+    "2014/04/09/write-the-code-spread-the-love-change-the-world/": "wwdc-14-spread-the-love",
+    "2014/05/09/tehilim-in-the-post-pc-era/": "tehilim-post-pc-era",
     "2015/02/04/you-may-ask-yourself-how-can-i-start-developing-apps-and-i-can-answer/": "how-i-learned-to-code",
+    "2015/08/09/empathy/": "empathy",
+    "2015/10/21/introduction-to-fastlane-and-integrating-it-with-circleci-to-deploy-beta-with-crashlytics-fabric/": "integrating-fastlane-with-circle-ci",
+    "2016/01/31/thoughts-on-the-parse-shutdown-how-the-terrible-news-became-the-best-thing-to-happen-to-my-app/": "parse-shutdown",
+    "2016/03/31/migrating-my-data-from-parse/": "migrating-my-parse-data",
+    "2016/06/05/wwdc-16-past-and-future/": "wwdc16-past-and-future",
 ]
 
 final class Routes: RouteCollection {
